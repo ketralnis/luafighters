74002fec7f3d89bb3798cf14b3bdb77637caa890
David King <dking@ketralnis.com>
2015-07-21 17:37:02 -0700

    Lots more progress on game engine

--- luafighters/board.py
+++ luafighters/board.py
@@ -1,13 +1,7 @@
 import random
 from collections import namedtuple
 
-CellDescriptor = namedtuple('CellDescriptor', 'x y cell')
-
-
-class Ships(object):
-    def __init__(self, owner, count):
-        self.owner = owner
-        self.count = count
+CellRef = namedtuple('CellRef', 'x y cell')
 
 
 class Planet(object):
@@ -19,7 +13,7 @@ class Planet(object):
 class Cell(object):
     def __init__(self):
         self.planet = None
-        self.shipss = []
+        self.ships = {}
 
 
 Order = namedtuple('Order', 'source_x source_y shipcount dest_x dest_y')
@@ -27,6 +21,7 @@ Order = namedtuple('Order', 'source_x source_y shipcount dest_x dest_y')
 
 class Board(object):
     def __init__(self, players, width, height):
+        self.turn = players[0]
         self.players = players
         self.width = width
         self.height = height
@@ -40,6 +35,32 @@ class Board(object):
             cells.append(row)
 
         self.cells = cells
+        self.orders = {player: [] for player in self.players}
+
+    def planets(self):
+        for y in xrange(self.height):
+            for x in xrange(self.width):
+                cell = self.cell_at(x, y)
+                if cell.planet:
+                    yield CellRef(x, y, cell)
+
+    def iterate(self):
+        for y in xrange(self.height):
+            for x in xrange(self.width):
+                cell = self.cell_at(x, y)
+                yield CellRef(x, y, cell)
+
+    def cell_at(self, x, y):
+        return self.cells[y][x]
+
+    @staticmethod
+    def normalize_ships(cell):
+        """
+	take a list of Ships and make sure empty ones are removed
+        """
+        cell.ships = {player: count
+                      for (player, count) in cell.ships.items()
+                      if count > 0}
 
     def random_empty(self):
         while True:
@@ -48,62 +69,68 @@ class Board(object):
             cell = self.cell_at(x, y)
 
             if not self.cell_at(x, y).planet:
-                return CellDescriptor(x, y, cell)
-
-    def cell_at(self, x, y):
-        return self.cells[y][x]
+                return CellRef(x, y, cell)
 
     def to_ascii(self):
-        collens = {x: len(' w(100), ' + ','.join('100' for x in self.players)+ ' ')
-                   for x in xrange(self.width)}
+        import termcolor
+        colours = {player: colour
+                   for (player, colour)
+                   in zip(self.players, 'red cyan white green'.split())}
+        coloured = lambda p, s: termcolor.colored(s, colours[p])
 
-        printrows = []
-
-        for rownum, row in enumerate(self.cells):
-            printrow = []
-
-            for colnum, cell in enumerate(row):
-                if cell.planet:
-                    planetstr = '%s(%d)' % (cell.planet.owner[0], cell.planet.size)
-                else:
-                    planetstr = ' '
-
-                shipsbyowner = [sum([ss.count for ss in cell.shipss
-                                     if ss.owner == p])
-                                for p in self.players]
-
-                if any(shipsbyowner):
-                    shipstr = ','.join(map(str, shipsbyowner))
-                else:
-                    shipstr = ''
-
-                printstr = ' '.join((' ', planetstr, ' ', shipstr, ' '))
-                printrow.append(printstr)
-                collens[colnum] = max(collens[colnum], len(printstr))
-
-            printrows.append(printrow)
+        collens = len(' w(100), ' + ','.join('100' for x in self.players)+' ')
 
         buf = []
 
-        for rownum, row in enumerate(printrows):
+        for rownum, row in enumerate(self.cells):
             buf.append(str(rownum))
-            buf.append('-'*(sum(collens.values())+len(collens)-len(str(rownum))))
+            buf.append('-'*(collens*self.width+self.width))
             buf.append('\n')
 
-            for colnum, col in enumerate(row):
+            for colnum, cell in enumerate(row):
                 if colnum != 0:
                     buf.append('|')
 
-                buf.append(col)
-                buf.append(' '*(collens[colnum]-len(col)))
+                spacers = collens
 
+                if cell.planet:
+                    planetstr = '%s(%d)' % (cell.planet.owner[0], cell.planet.size)
+                    spacers -= len(planetstr)
+                    buf.append(coloured(cell.planet.owner, planetstr))
+
+                    buf.append(' ')
+                    spacers -= len(' ')
+
+                if cell.ships:
+                    for ip, player in enumerate(self.players):
+                        if ip != 0:
+                            buf.append(',')
+                            spacers -= len(',')
+                        shipstr = str(cell.ships.get(player, 0))
+                        if cell.ships.get(player, 0):
+                            buf.append(coloured(player, shipstr))
+                        else:
+                            buf.append(shipstr)
+                        spacers -= len(shipstr)
+
+                buf.append(' '*spacers)
+            buf.append('\n')
+
+        for player in self.players:
+            playersum = 0
+            for _, _, cell in self.iterate():
+                playersum += cell.ships.get(player, 0)
+            buf.append(coloured(player, "%s: %d" % (player, playersum)))
             buf.append('\n')
 
         return ''.join(buf)
 
 
     @classmethod
-    def generate_board(cls, width=10, height=25, neutralplanets=12):
+    def generate_board(cls,
+                       players=['white', 'black'],
+                       width=10, height=24,
+                       neutralplanets=12):
         players = ['white', 'black']
         assert 'neutral' not in players
         players.append('neutral')
@@ -114,17 +141,15 @@ class Board(object):
 
         for player in players:
             myplanet = Planet(player, 100)
-            myships = Ships(player, 100)
             cell = board.random_empty().cell
             cell.planet = myplanet
-            cell.shipss.append(myships)
+            cell.ships[player] = 100
 
         for x in xrange(neutralplanets):
             neutralplanet = Planet('neutral', random.randint(1, 100))
-            neutralships = Ships('neutral', random.randint(1, 100))
             cell = board.random_empty().cell
             cell.planet = neutralplanet
-            cell.shipss.append(neutralships)
+            cell.ships['neutral'] = random.randint(1, 100)
 
         return board
 
--- luafighters/game.py
+++ luafighters/game.py
@@ -1,7 +1,197 @@
 #!/usr/bin/env python2.7
 
-from luafighters import board
+import random
+import logging
+import time
+from itertools import cycle
+import itertools
+
+from luafighters.board import Board, Order
+
+
+class Strategy(object):
+    def __init__(self, player):
+        self.player = player
+        self.state = None
+
+    def make_turn(self, board):
+        raise NotImplemented
+
+
+class NullStrategy(Strategy):
+    def make_turn(self, board):
+        return []
+
+
+class RandomStrategy(Strategy):
+    """
+    Every turn move some ships around at random
+    """
+
+    def make_turn(self, board):
+        orders = []
+
+        for x, y, cell in board.iterate():
+            playerships = cell.ships.get(self.player, 0)
+            if not playerships:
+                continue
+
+            moveships = random.randint(0, playerships)
+            dest_x = self.capped_random(x, 0, board.width-1)
+            dest_y = self.capped_random(y, 0, board.height-1)
+
+            if moveships == 0 or (dest_x, dest_y) == (0, 0):
+                # no need to issue noop orders
+                continue
+
+            # n.b. this can issue invalid orders, which the turn pump should disregard
+            orders.append(Order(x, y, moveships, x+dest_x, y+dest_y))
+
+        return orders
+
+    @staticmethod
+    def capped_random(x, min_, max_):
+        choices = [0]
+        if x-1 >= min_:
+            choices.append(-1)
+        if x+1 <= max_:
+            choices.append(1)
+        return random.choice(choices)
+
+
+def turns(board):
+    playerswithturns = filter(lambda x: x!='neutral', board.players)
+    for turncount, player in enumerate(cycle(playerswithturns)):
+        if player == 'neutral':
+            # the neutral player doesn't go
+            logging.debug("Skipping neutral turn")
+            continue
+
+        if not any(cd.cell.planet.owner == player for cd in board.planets()):
+            # they are dead, they don't get to go
+            logging.info("Skipping %r", player)
+            continue
+
+        # tell the caller whose turn it is and they give us a list of orders
+        orders = (yield turncount, player)
+
+        if orders is None:
+            continue
+
+        orders, = orders
+        
+        valid_orders = []
+
+        # apply those orders to the board
+        for order in orders:
+            try:
+                source = board.cell_at(order.source_x, order.source_y)
+                dest = board.cell_at(order.dest_x, order.dest_y)
+            except IndexError:
+                logging.warning("Ignoring order for non-existant cell %r:(%d,%d -> %d,%d)",
+                                player, order.source_x, order.source_y,
+                                order.dest_x, order.dest_y)
+                continue
+
+            if order.shipcount > source.ships.get(player, 0):
+                logging.warning("Downgrading shipcount %r %d -> %d %r",
+                                player, order.shipcount, source.ships.get(player, 0),
+                                order)
+                order = order._replace(shipcount=source.ships.get(player, 0))
+
+            if order.shipcount == 0:
+                logging.warning("Skipping noop move for %r", player)
+                continue
+
+            valid_orders.append(order)
+
+	# now actually process the move requests. we do this in a
+	# separate loop to keep earlier orders from affecting layer
+	# ones
+        for source_x, source_y, shipcount, dest_x, dest_y in valid_orders:
+            # process the movement order
+            source = board.cell_at(source_x, source_y)
+            dest = board.cell_at(dest_x, dest_y)
+
+            source.ships[player] -= shipcount
+            dest.ships.setdefault(player, 0)
+            dest.ships[player] += shipcount
+
+            board.normalize_ships(source)
+            board.normalize_ships(dest)
+
+        # do all fights
+        for x, y, cell in board.iterate():
+            # planet production. each planet gets 10% of its
+            # size in ships every turn
+            planet = cell.planet
+            if planet and planet.owner != 'neutral':
+                cell.ships[planet.owner] = cell.ships.get(planet.owner, 0) + planet.size/10
+
+            # ships fight
+            if len(cell.ships) > 1:
+                orig_ships = sorted(cell.ships.items())
+                # half of all ships fight per turn
+                fighting_ships = [(ships/2, fighter)
+                                  for (fighter, ships)
+                                  in cell.ships.items()]
+                fighting_ships.sort()
+                largest_army, victor = fighting_ships[-1]
+                next_army, next_victor = fighting_ships[-2]
+                # everyone loses half of their ships, except
+                # the victor who keeps the difference between him
+                # and the next guy
+                newships = {fighter: ((ships/2+(largest_army-next_army))
+                                      if fighter == victor
+                                      else ships/2)
+                            for (fighter, ships) in cell.ships.items()}
+                cell.ships = newships
+                board.normalize_ships(cell)
+
+            # planet fight: you own a planet if you're the only one with ships on it
+            if planet and len(cell.ships) == 1 and planet.owner != cell.ships.keys()[0]:
+                planet.owner = cell.ships.keys()[0]
+
+            # cap shipcounts; should we do this?
+            cell.ships = {owner: min(count, 999)
+                          for (owner, count) in cell.ships.items()}
+
+        # check for victory
+        if all(cd.cell.planet.owner in (player, 'neutral')
+               for cd in board.planets()):
+            # the game is over, this player has won
+            logging.info("Terminating with victory: %r", board.planets()[0].owner)
+            return
+
+
 
 def main():
-    board = board.Board.generate_board()
-    print board.to_ascii()
+    strategies = {
+        'white': RandomStrategy('white'),
+        'black': NullStrategy('black'),
+    }
+    board = Board.generate_board(players = strategies.keys())
+    turns_pump = turns(board)
+
+    turns_pump.next() # prime the coroutine
+
+    orders = []
+
+    try:
+        while True:
+            turncount, player = turns_pump.send((orders,))
+
+            orders = strategies[player].make_turn(board)
+
+            print "%s's turn #%d" % (player, turncount)
+            print board.to_ascii()
+
+            time.sleep(1)
+
+    except StopIteration:
+        winner = board.planets()[0].owner
+
+    print '*'*20, winner, 'wins!'
+
+if __name__ == '__main__':
+    main()
