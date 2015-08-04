a1c79142ed10bc14c12585903a347ed0a2378dd2
David King <dking@ketralnis.com>
2015-08-04 16:44:20 -0700

    make pep8 happy

--- setup.py
+++ setup.py
@@ -3,29 +3,30 @@
 from setuptools import setup, find_packages, Extension
 
 _executor = Extension('luafighters._executor',
-                   define_macros = [('MAJOR_VERSION', '1'),
-                                    ('MINOR_VERSION', '0')],
-                   libraries = ['lua', 'm'],
-                   library_dirs = ['/opt/local/lib'],
-                   include_dirs = ['/opt/local/include', '/usr/include/lua5.2', './c'],
-                   sources = ['c/_executormodule.c'],
-                   extra_compile_args=['-std=c99'],
-                   )
+                      define_macros=[('MAJOR_VERSION', '1'),
+                                     ('MINOR_VERSION', '0')],
+                      libraries=['lua', 'm'],
+                      library_dirs=['/opt/local/lib'],
+                      include_dirs=[
+                        '/opt/local/include', '/usr/include/lua5.2', './c'],
+                      sources=['c/_executormodule.c'],
+                      extra_compile_args=['-std=c99'],
+                      )
 
 setup(
-    name = 'luafighters',
-    version = '1.0',
-    description = 'Lua Fighters: Battle of the Luas',
-    author = 'David King',
-    author_email = 'dking@ketralnis.com',
-    #url = 'https://docs.python.org/extending/building',
-    long_description = "Players write Lua scripts to compete with each other",
-    ext_modules = [_executor],
+    name='luafighters',
+    version='1.0',
+    description='Lua Fighters: Battle of the Luas',
+    author='David King',
+    author_email='dking@ketralnis.com',
+    #url='https://docs.python.org/extending/building',
+    long_description="Players write Lua scripts to compete with each other",
+    ext_modules=[_executor],
     packages=find_packages(),
     package_data={'luafighters': ['lua/*.lua', 'static/*', 'mako/*']},
     zip_safe=False,
     include_package_data=True,
-    install_requires = [
+    install_requires=[
         'termcolor',
         'flask',
         'mako',
